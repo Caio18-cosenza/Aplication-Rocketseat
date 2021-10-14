28ae049c42b64ad95586d5e5b842b5bda7e54702
Caio18-cosenza <caiiocosenza2020@gmail.com>
2021-10-14 17:23:37 -0300

    First commit animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Route, BrowserRouter } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+import { AuthContentextProvider } from './context/AuthContext';
+
 function App() {
-  return <h1> Hello World </h1>;
+  return (
+    <BrowserRouter>
+      <AuthContentextProvider>
+        <Route path='/' exact component={Home} />
+        <Route path='/rooms/new' exact component={NewRoom} />
+      </AuthContentextProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
